622dea955ff18f9fa42598e8c82c303f526a7e76
Allan Dayrell <allan.dayrell@gmail.com>
2024-05-22 16:03:19 -0300

    feat: migração music model

---
+++ prisma/migrations/20240522185502_model_music/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Music" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "album" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    CONSTRAINT "Music_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+);
