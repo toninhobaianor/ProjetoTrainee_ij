16fca8cb22215e438c4a7d734d78688aeed62de4
Allan Dayrell <allan.dayrell@gmail.com>
2024-05-22 16:37:44 -0300

    Revert "feat: função create do MusicService"
    This reverts commit 7c84d05811ba44e1d8b0a94a74a6ce723e57acc3.

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import UserService from "./src/domains/User/service/Userservice"
-import MusicService from "./src/domains/User/service/MusicService"
 
 async function main(){
   const body = {
@@ -11,17 +10,8 @@ async function main(){
     tem_privilegio: "admin"
   }
 
-  //const user = await UserService.create(body);
-  //console.log(user);
-
-  const music = await MusicService.create({
-    album: "xuxa, só as melhores",
-    genre: "genero de musica",
-    name: "axux",
-    authorId: 1,
-    id: 1,
-  });
-  console.log(music);
+  const user = await UserService.create(body);
+  console.log(user);
 }
 
 main();
