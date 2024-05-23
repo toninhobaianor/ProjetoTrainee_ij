1bab32f65781f33fdc2699581e8b6a5fe4ab8c5f
Allan Dayrell <allan.dayrell@gmail.com>
2024-05-23 09:52:21 -0300

    fix: correção do endereço de MusicService

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import UserService from "./src/domains/User/service/Userservice"
+import MusicService from "./src/domains/Music/service/MusicService"
 
 async function main(){
   /*const body = {
@@ -13,9 +14,18 @@ async function main(){
   const user = await UserService.create(body);
   console.log(user);*/
 
-  const email = "tonin1@gmail.com";
-  const user = await UserService.update(email,"senhanova1",3);
-  console.log(user);
+  //const email = "tonin1@gmail.com";
+  //const user = await UserService.update(email,"senhanova1",3);
+  //console.log(user);
+
+  const music = await MusicService.create({
+    album: "music",
+    authorId: 1,
+    genre: "music",
+    id: 3,
+    name: "music",
+  });
+  console.log(music);
 }
 
 main();
