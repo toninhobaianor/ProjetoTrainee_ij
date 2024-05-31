f75aa77c5e053327ebc102ddeb8dc9039c8c7c14
Antonio Alberto <antoniomaster@gmail.com>
2024-05-31 19:42:52 -0300

    feat: criação das rotas de UserMusic

---
+++ src/domains/UserMusic/controllers/index.ts
@@ -0,0 +1,28 @@
+import { User } from "@prisma/client";
+import { Music } from "@prisma/client";
+import { Router, Request, Response, NextFunction } from "express";
+import UserMusicService from "../service/UserMusicService";
+
+const router = Router();
+
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const user: User = req.body;
+        const music: Music = req.body;
+		const usermusic = await UserMusicService.createUserMusic(music,user);
+		res.json(usermusic);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.delete("/delete", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+        const user: User = req.body;
+        const music: Music = req.body;
+		const usermusic = await UserMusicService.deleteUserMusic(user,music);
+		res.json(user);
+	} catch (error) {
+		next(error);
+	}
+});
