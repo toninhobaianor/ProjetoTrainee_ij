1818f91ba7030722f780dbcfd27eefc9c39aa4ac
Christian Oliveira <chrisfrlps1@gmail.com>
2024-06-23 15:35:46 -0300

    Testes das funções do artista
    Correção das funçoes de AuthorService, retirando try catch  e criação dos testes para as funções do artista

--- src/domains/Author/service/AuthorService.test.ts
+++ src/domains/Author/service/AuthorService.test.ts
@@ -99,12 +99,222 @@ describe("readAll", () => {
 	});
 });
 
+describe("ReadByID", () => {
+	const artist = 
+		{
+			id: 1, 		
+			Author: "Artista teste",
+			StreamCount: 1000,
+			photo: null,
+			musics: [
+				{
+					id: 1,
+					name: "Musica 1",
+					genre: "Sertanejo",
+					album: "As Melhores",
+					authorId: 1
+				},
+				{
+					id: 2,
+					name: "Musica 2",
+					genre: "Funk",
+					album: "Top Hits",
+					authorId: 1
+				}
+			]
+		};
+
+	test("deve ler as informações do banco de dados através de um ID ==> retornar as informações dos artistas corretamente pelo ID", async () => {
+		prismaMock.author.findUnique.mockResolvedValue(artist);
+		const result = await AuthorService.ReadByID(artist.id);
+		expect(result).toEqual(artist);
+	});
+	test("deve ler um id de um artista que nao esteja no banco ==> gerar erro", async () => {
+		prismaMock.author.findUnique.mockResolvedValue(null);
+		
+		await expect(AuthorService.ReadByID(artist.id)).rejects.toThrow(
+			new Error("Sua pesquisa não gerou resultados. O ID: '" + artist.id + "' não está na nossa base de dados."));
+
+	});
+
+	test("deve ler um id que nao seja numero ==> gerar erro", async () => {
+		const invalidId = "invalid_id";
+		
+		await expect(AuthorService.ReadByID(invalidId as unknown as number)).rejects.toThrow(
+			new Error("O parâmetro deve ser um número."));
+
+	});
+});
+
+
+describe("ReadByMusic", () => {
+	const artist = 
+		{
+			id: 1, 		
+			Author: "Artista teste",
+			StreamCount: 1000,
+			photo: null,
+			musics: [
+				{
+					id: 1,
+					name: "Musica 1",
+					genre: "Sertanejo",
+					album: "As Melhores",
+					authorId: 1
+				},
+				{
+					id: 2,
+					name: "Musica 2",
+					genre: "Funk",
+					album: "Top Hits",
+					authorId: 1
+				}
+			]
+		};
+
+	test("deve ler o nome de uma música ==> retornar as informações dos artistas dono da musica corretamente", async () => {
+		prismaMock.author.findMany.mockResolvedValue([artist]);
+		
+		const result = await AuthorService.ReadByMusic(artist.musics[0].name);
+		expect(result).toEqual([artist]);
+	});
+
+	test("deve ler o nome de uma  musica que nao esteja no banco ==> gerar erro", async () => {
+		prismaMock.author.findFirst.mockResolvedValue(null);
+		
+		await expect(AuthorService.ReadByMusic(artist.musics[1].name)).rejects.toThrow(
+			new Error("Sua pesquisa não gerou resultados. A música: '" + artist.musics[1].name + "' não está na nossa base de dados."));
+
+	});
+
+	test("deve ler um nome em branco ==> gerar erro", async () => {
+		prismaMock.author.findFirst.mockResolvedValue(null);
+		
+		await expect(AuthorService.ReadByMusic("")).rejects.toThrow(
+			new Error("O parâmetro não pode ser uma string vazia."));
+
+	});
+});
 
 
 
+describe("ReadByIDMusic", () => {
+	const artist = 
+		{
+			id: 1, 		
+			Author: "Artista teste",
+			StreamCount: 1000,
+			photo: null,
+			musics: [
+				{
+					id: 1,
+					name: "Musica 1",
+					genre: "Sertanejo",
+					album: "As Melhores",
+					authorId: 1
+				},
+				{
+					id: 2,
+					name: "Musica 2",
+					genre: "Funk",
+					album: "Top Hits",
+					authorId: 1
+				}
+			]
+		};
+
+	test("deve ler o ID de uma música ==> retornar as informações dos artistas dono da musica corretamente", async () => {
+		prismaMock.author.findFirst.mockResolvedValue(artist);
+		
+		const result = await AuthorService.ReadByIDMusic(artist.musics[0].id);
+		expect(result).toEqual(artist);
+	});
+
+	test("deve ler um id de uma  musica que nao esteja no banco ==> gerar erro", async () => {
+		prismaMock.author.findFirst.mockResolvedValue(null);
+		
+		await expect(AuthorService.ReadByIDMusic(artist.musics[1].id)).rejects.toThrow(
+			new Error("Sua pesquisa não gerou resultados. O ID: '" + artist.musics[1].id + "' não está na nossa base de dados."));
+
+	});
+
+	test("deve ler um id que nao seja numero ==> gerar erro", async () => {
+		const invalidId = "invalid_id";
+		
+		await expect(AuthorService.ReadByIDMusic(invalidId as unknown as number)).rejects.toThrow(
+			new Error("O parâmetro deve ser um número."));
+
+	});
+});
+
+describe("UpdateArtist", () => {
+	const artist = 
+		{
+			id: 10, 		
+			Author: "Artista teste",
+			StreamCount: 1000,
+			photo: null,
+			musics: [
+				{
+					id: 1,
+					name: "Musica 1",
+					genre: "Sertanejo",
+					album: "As Melhores",
+					authorId: 1
+				},
+				{
+					id: 2,
+					name: "Musica 2",
+					genre: "Funk",
+					album: "Top Hits",
+					authorId: 1
+				}
+			]
+		};
+	const invalidAuthor = {
+		id: 100,	
+		StreamCount: 1000,
+		photo: null
+	};
+
+	test("deve ler o ID de um Artista ==> Atualiza as informações do artista", async () => {
+		prismaMock.author.findUnique.mockResolvedValue(artist);
+
+		await AuthorService.updateArtist(10, artist);
+		
+		const result = await AuthorService.ReadByID(10);
+		expect(result).toEqual(artist);
+	});
 
 
 
+	test("deve ler um id de um artista que nao esteja no banco ==> gerar erro", async () => {
+		prismaMock.author.findUnique.mockResolvedValue(null);
+		
+		await expect(AuthorService.updateArtist(artist.id, artist)).rejects.toThrow(
+			new Error("Sua pesquisa não gerou resultados. O ID: '" + artist.id + "' não está na nossa base de dados."));
+
+	});
+
+	test("deve ler um id que nao seja numero ==> gerar erro", async () => {
+		const invalidId = "invalid_id";
+		
+		await expect(AuthorService.updateArtist(invalidId as unknown as number, artist)).rejects.toThrow(
+			new Error("O parâmetro deve ser um número."));
+
+	});
+
+	test("tenta atualizar artista com parametro errado ==> gera erro", async () => {
+		prismaMock.author.findUnique.mockResolvedValue(null);
+
+		await expect(AuthorService.createArtist(invalidAuthor as unknown as Author)).rejects.toThrow(
+			new Error("Parâmetros inválidos fornecidos.")
+		);
+		expect(prismaMock.author.delete).not.toHaveBeenCalled();
+	});
+
+
+});
 
 describe("deleteAuthor", () => {
 	const author = {
@@ -129,7 +339,7 @@ describe("deleteAuthor", () => {
 	test("tenta deletar artista inexistente ==> gera erro", async () => {
 		prismaMock.author.findUnique.mockResolvedValue(null);
 		await expect(AuthorService.deleteArtist(0)).rejects.toThrow(
-			new Error("Não foi possível deletar o artista.")
+			new Error("Sua pesquisa não gerou resultados. O ID: '0' não está na nossa base de dados.")
 		);
 
 		expect(prismaMock.author.findUnique).toHaveBeenCalledWith({ where: { id: 0 } });
--- src/domains/Author/service/AuthorService.ts
+++ src/domains/Author/service/AuthorService.ts
@@ -42,97 +42,92 @@ class AuthorService{
         
 
 	async ReadByID(id: number){
-		try{
-			const result = await prisma.author.findUnique({
-				where: {id: Number(id)},
-			});
-			if(result){
-				return result;
-			}else{
-				throw new Error("Sua pesquisa não gerou resultados. O ID: '" + id + "' não está na nossa base de dados.");
-			}
+		if(isNaN(id)){
+			throw new Error("O parâmetro deve ser um número.");
+		}
 
-		}catch(error){
-			console.log(error);
-		} 
-
+		const result = await prisma.author.findUnique({
+			where: {id: Number(id)},
+		});
+		if(result){
+			return result;
+		}else{
+			throw new Error("Sua pesquisa não gerou resultados. O ID: '" + id + "' não está na nossa base de dados.");
+		}
 	}
 
 	async ReadByMusic(music: string ){
-		try{
-			const result = await prisma.author.findFirst({
-				where: {
-					musics: {
-						some: {
-							name: music,
-						},
 
+		if(music.trim() === ""){
+			throw new Error("O parâmetro não pode ser uma string vazia.");
+		}
+		const result = await prisma.author.findMany({
+			where: {
+				musics: {
+					some: {
+						name: music,
 					},
 				},
-			});
-			if(result){
-				return result;
-			}else{
-				throw new Error("Sua pesquisa não gerou resultados. A música: '" + music + "' não está na nossa base de dados.");
-			}
-
-
-
-		}catch(error){
-			console.log(error);
-		} 
+			},
+		});
+		if(result && result.length > 0){
+			return result;
+		}else{
+			throw new Error("Sua pesquisa não gerou resultados. A música: '" + music + "' não está na nossa base de dados.");
+		}
 	}
 
-	async ReadByIDMusic(id: number ){
-		try{
-			const result = await prisma.author.findFirst({
-				where: {
-					musics: {
-						some: {
-							id: Number(id),
-						},
 
+	async ReadByIDMusic(id: number ){
+		
+		if(isNaN(id)){
+			throw new Error("O parâmetro deve ser um número.");
+		}
+		const result = await prisma.author.findFirst({
+			where: {
+				musics: {
+					some: {
+						id: Number(id),
 					},
+
 				},
-			});
-			if(result){
-				return result;
-			}else{
-				throw new Error("Sua pesquisa não gerou resultados. O ID: '" + id + "' não está na nossa base de dados.");
-			}
-		}catch(error){
-			console.log(error);
-		} 
+			},
+		});
+		if(result){
+			return result;
+		}else{
+			throw new Error("Sua pesquisa não gerou resultados. O ID: '" + id + "' não está na nossa base de dados.");
+		}
 	}
 
 	//UPDATE
 	async updateArtist(id: number, body: Author){
-		try{
-			const author = await this.ReadByID(id);
-			console.log(author);
-
-			if(author){
-				const result = await prisma.author.update({
-					data:{
-						Author: body.Author,
-						photo: body.photo,
-						StreamCount: body.StreamCount,
-
-					}, where:{
-						id: id,
-					},
-				});
-				return result;
-
-			}else{
-				throw new Error("Não foi possível Atualizar o artista.");
-			}
+		if (!body || !body.Author ){
+			throw new Error("Parâmetros inválidos fornecidos.");
+		}
+		
+		const author = await this.ReadByID(id);
 
 
+		if(author){
+			const result = await prisma.author.update({
+				data:{
+					Author: body.Author,
+					photo: body.photo,
+					StreamCount: body.StreamCount,
 
-		}catch(error){
-			console.log(error);
-		} 
+				}, where:{
+					id: id,
+				},
+			});
+			return result;
+
+		}else{
+			throw new Error("Não foi possível Atualizar o artista.");
+		}
+
+
+
 	}
 
 
