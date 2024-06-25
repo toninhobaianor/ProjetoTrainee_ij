9abf3713f1257cd6707f3c505b56589d4373a3b3
Antonio Alberto <85882536+toninhobaianor@users.noreply.github.com>
2024-06-25 13:38:10 -0300

    fix: concertando o index

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-app.listen(process.env.Port, () => {
+app.listen(process.env.PORT, () => {
 	console.log("Servidor hosteado na porta " + process.env.PORT); 
 });
